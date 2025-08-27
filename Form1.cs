eb4bc8bab431e28e150590c3667ebd5f928c0491
Omar <o.alaiydi@hotmail.com>
2025-08-27 09:21:29 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Symmetric_Encryption
+{
+    public partial class Form1 : Form
+    {
+
+        private const int _KEY = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Decrypt()
+        {
+            string Text1 = txtDecryptedText.Text;
+            string Text2 = "";
+            for (int i = 0; i < Text1.Length; i++)
+            {
+                Text2 += Convert.ToChar((int)Text1[i] - _KEY);
+            }
+
+            txtOriginalText2.Text = Text2;
+        }
+
+        private void Encrypt()
+        {
+            string Text1 = txtOriginalText1.Text;
+            string Text2 = "";
+            for (int i = 0; i < Text1.Length; i++)
+            {
+                Text2 += Convert.ToChar((int)Text1[i] + _KEY);
+            }
+
+            txtEncryptedText.Text = Text2;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            Encrypt();
+        }
+
+        private void btnReset1_Click(object sender, EventArgs e)
+        {
+            txtOriginalText1.Clear();
+            txtEncryptedText.Clear();
+        }
+
+        private void btnSave1_Click(object sender, EventArgs e)
+        {
+
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.ShowHelp = true;
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Title = "Save Encrypted Data";
+
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, txtEncryptedText.Text);
+                MessageBox.Show(saveFileDialog1.FileName + " Saved Successfully.",
+                    "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            Decrypt();
+        }
+
+        private void btnReset2_Click(object sender, EventArgs e)
+        {
+            txtDecryptedText.Clear();
+            txtOriginalText2.Clear();
+        }
+
+        private void btnSave2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.ShowHelp = true;
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Title = "Save Decrypted Data";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, txtOriginalText2.Text);
+                MessageBox.Show(saveFileDialog1.FileName + " Saved Successfully.",
+                    "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Symmetric Encryption (C# WinForms)
+
+A simple Windows Forms app that demonstrates symmetric encryption and decryption.
+
+## How to run
+1. Open `Symmetric Encryption.sln` in Visual Studio.
+2. Build the project.
+3. Run the app.
+
+## Features
+- Encrypt text (left panel).
+- Decrypt text (right panel).
+- Reset and Save buttons.
+
+## Screenshot
